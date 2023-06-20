020711157d4a4e8af36d6a3e96ffd08e8c6924e9
博闻 <bw1863@outlook.com>
2023-06-20 22:52:06 +0800

    修改
    Former-commit-id: bb54c6343ce43d152d8c1e371eb85ae2385de446

--- 机器学习项目/深度图像网络强化学习/environment.py
+++ 机器学习项目/深度图像网络强化学习/environment.py
@@ -1,11 +1,11 @@
 '''
-get_data(code, start, long), 在线下载数据, 输入例如 ('sh.600000', '2017-12-31', '100')
+get_data(code, start, long), ??????, ???? ('sh.600000', '2017-12-31', '100')
 
-make_img(), 返回k线图, 如无调试需要可以不管, 执行reset()方法自动调用
+make_img(), ??k??, ??????????, ??reset()??????
 
-reset()), 初始化一个环境, 给出obs(, info), 即一个初始k线图的数组形式(和一个信息备注)
+reset()), ???????, ??obs(, info), ?????k???????(???????)
 
-step(action), 输入一个动作, 返回 next_state, reward, done
+step(action), ??????, ?? next_state, reward, done
 '''
 
 
@@ -19,7 +19,7 @@ import numpy as np
 
 def fig2data(fig):
     '''
-    输入图片, 输出ndarray数组
+    ????, ??ndarray??
     '''
     import PIL.Image as Image
     # draw the renderer
@@ -37,13 +37,13 @@ def fig2data(fig):
 
 def get_data(self, code, start, long):
     '''
-    无初始数据, 需要在线下载时使用该方法
+    ?????, ????????????
     
     code: 'sh.600000'
     
     start: '2017-12-31'
     
-    long: 整数
+    long: ??
     '''
     year, month, day = map(int, start.split('-'))
     date = datetime(year, month, day)
@@ -56,43 +56,43 @@ def get_data(self, code, start, long):
             start_date=start, end_date=end, frequency="d", adjustflag="2")
         print('query_history_k_data_plus respond  error_msg:'+rs.error_msg)
 
-        #### 打印结果集 ####
+        #### ????? ####
         data_list = []
         while (rs.error_code == '0') & rs.next():
-            # 获取一条记录，将记录合并在一起
+            # ??????,????????
             data_list.append(rs.get_row_data())
         data = pd.DataFrame(data_list, columns=rs.fields)
         data.to_csv("k_data.csv", index=False)
-        print('数据已下载到同目录下 k_data.csv 文件')
-        #### 登出系统 ####
+        print('?????????? k_data.csv ??')
+        #### ???? ####
         bs.logout()
     except:
-        print('在线获取数据时发生错误')
+        print('???????????')
 
 class stock:
     def __init__(self):
         
         print('v-1.9')
-        print('get_data(code, start, long) 在线下载数据')
-        print('reset() 返回一个初始k线图')
-        print('step(action) 采取动作action')
+        print('get_data(code, start, long) ??????')
+        print('reset() ??????k??')
+        print('step(action) ????action')
 
         self.my_color = mpf.make_marketcolors(
-            up="red",  # 上涨K线的颜色
-            down="green",  # 下跌K线的颜色
-            edge="black",  # 蜡烛图箱体的颜色
-            wick="black"  # 蜡烛图影线的颜色
+            up="red",  # ??K????
+            down="green",  # ??K????
+            edge="black",  # ????????
+            wick="black"  # ????????
         )
 
-        # 自定义风格
+        # ?????
         self.my_style = mpf.make_mpf_style(
             marketcolors=self.my_color,
         )
 
     def make_img(self, df, figure_show=0):
         '''
-        df: 数据, 以时间为索引, open, high, low, close, (volumn)
-        figure_show: 是否返回图片, 默认0, 返回数组
+        df: ??, ??????, open, high, low, close, (volumn)
+        figure_show: ??????, ??0, ????
         '''
 
         fig, ax = mpf.plot(df.iloc[self.order - self.k_num:self.order], type='candle', style=self.my_style, 
@@ -105,31 +105,31 @@ class stock:
         
     def reset(self, offline=1, k_num=40, info_return=0, figure_show=0):
         '''
-        flag: 有本地数据传1, 没有本地数据传0
+        flag: ??????1, ???????0
         
-        k_num: 时间周期, 即一个图像多少k线
+        k_num: ????, ???????k?
         
-        episods: 记录回合数
+        episods: ?????
         
-        info_return: 额外信息, 默认不显示
+        info_return: ????, ?????
         
-        figure_show: 返回图像而不是数组, 默认0, 返回数组
+        figure_show: ?????????, ??0, ????
         '''
         
-        if offline == 1: # 有本地数据
+        if offline == 1: # ?????
             self.data = pd.read_csv('k_data.csv')
         else:
-            code= input('输入代码: 如 sh.600000')
-            start = input('起始时间: 如 2020-12-01')
-            long = int(input('持续天数: 如 100'))
+            code= input('????: ? sh.600000')
+            start = input('????: ? 2020-12-01')
+            long = int(input('????: ? 100'))
             self.data = get_data(str(code), str(start), long)
             
-        self.reward_list = [] # 奖励历史
-        self.total_reward = 0 # 总奖励
-        self.amount = 0 # 总持仓
-        self.amount_history = [] # 交易记录
-        self.order = np.random.randint(k_num, int(self.data.shape[0] * 0.9)) #### 最终时间, 每回合加一
-        self.k_num = k_num # 固定的周期 (几根k线)
+        self.reward_list = [] # ????
+        self.total_reward = 0 # ???
+        self.amount = 0 # ???
+        self.amount_history = [] # ????
+        self.order = np.random.randint(k_num, int(self.data.shape[0] * 0.9)) #### ????, ?????
+        self.k_num = k_num # ????? (??k?)
         self.start_place = self.order
         
         self.data.index = pd.DatetimeIndex(self.data['date'])
@@ -143,29 +143,29 @@ class stock:
     
     def step(self, action):
         '''
-        0:买入1, 1:全做多10, 2:不变0, 3:卖出1, 4:全做空-10
+        0:??1, 1:???10, 2:??0, 3:??1, 4:???-10
         
         return next_state, reward, done
         '''
-        if self.amount < 10 and action == 0: # 全做多
+        if self.amount < 10 and action == 0: # ???
             self.amount += 1
-        elif action == 1: # 全仓
+        elif action == 1: # ??
             self.amount = 10
-        elif self.amount > -10 and action == 3: # 允许做空10次
+        elif self.amount > -10 and action == 3: # ????10?
             self.amount -= 1
-        elif action == 4: # 全做空
+        elif action == 4: # ???
             self.amount = -10
         elif action == 2:
             pass
         
-        self.amount_history.append(self.amount) # 记录持仓
+        self.amount_history.append(self.amount) # ????
         
         reward = ((self.data['close'][self.order] - self.data['close'][self.order-1]) \
-                  / self.data['close'][self.order-1]) * self.amount * 10 # 持仓量乘以涨跌比例*10
+                  / self.data['close'][self.order-1]) * self.amount * 10 # ?????????*10
         
-        self.reward_list.append(self.total_reward) # 记录历史总奖励
+        self.reward_list.append(self.total_reward) # ???????
         
-        if self.total_reward < -30 or self.order - self.start_place >= 100: #### 分数小于-30或者次数大于100结束
+        if self.total_reward < -20 or self.order - self.start_place >= 100: #### ????-20??????100??
             done = 1
         else:
             done = 0
