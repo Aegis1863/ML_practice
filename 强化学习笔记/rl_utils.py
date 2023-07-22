239e2113418e2d2449aa6bc3e40b14620a3289ff
博闻 <bw1863@outlook.com>
2023-07-22 15:09:06 +0800

    add PPO and some changes

--- 强化学习笔记/rl_utils.py
+++ 强化学习笔记/rl_utils.py
@@ -34,17 +34,17 @@ def moving_average(a, window_size):
     end = (np.cumsum(a[:-window_size:-1])[::2] / r)[::-1]
     return np.concatenate((begin, middle, end))
 
-def train_on_policy_agent(env, agent, s_epoch, total_epoch, s_episode, total_episode, reward_list, ckp_path):
+def train_on_policy_agent(env, agent, s_epoch, total_epochs, s_episode, total_episodes, return_list, ckp_path):
     '''
-    在线策略, 没有经验池
+    在线策略, 没有经验池, 仅限演员评论员框架
     '''
     best_score = 0
-    if not reward_list:
-        reward_list = []
-    for epoch in range(s_epoch, total_epoch):
-        with tqdm(total=(total_episode - s_episode), desc='<%d/%d>' % (epoch + 1, total_epoch), leave=False) as pbar:
-            for episode in range(s_episode, total_episode):
-                episode_reward = 0
+    if not return_list:
+        return_list = []
+    for epoch in range(s_epoch, total_epochs):
+        with tqdm(total=(total_episodes - s_episode), desc='<%d/%d>' % (epoch + 1, total_epochs), leave=False) as pbar:
+            for episode in range(s_episode, total_episodes):
+                episode_return = 0
                 transition_dict = {'states': [], 'actions': [], 'next_states': [], 'rewards': [],
                                    'dones': [], 'truncated': []}
                 state = env.reset()[0]
@@ -59,24 +59,24 @@ def train_on_policy_agent(env, agent, s_epoch, total_epoch, s_episode, total_epi
                     transition_dict['dones'].append(done)
                     transition_dict['truncated'].append(truncated)
                     state = next_state
-                    episode_reward += reward
-                reward_list.append(episode_reward)
+                    episode_return += reward
+                return_list.append(episode_return)
                 agent.update(transition_dict)
                 if (episode+1) % 10 == 0:
-                    pbar.set_postfix({'episode': '%d' % (total_episode * epoch + episode + 1),
-                                      'recent_return': '%.3f' % np.mean(reward_list[-10:])})
+                    pbar.set_postfix({'episode': '%d' % (total_episodes * epoch + episode + 1),
+                                      'recent_return': '%.3f' % np.mean(return_list[-10:])})
                     
-                if episode_reward > best_score:
+                if episode_return > best_score:
                     actor_best_weight = agent.actor.state_dict()
                     critic_best_weight = agent.critic.state_dict()
-                    best_score = episode_reward
+                    best_score = episode_return
                     
                 torch.save({
                 'epoch': epoch,
                 'episode': episode,
                 'actor_best_weight': actor_best_weight,
                 'critic_best_weight' : critic_best_weight,
-                'reward_list': reward_list,
+                'return_list': return_list,
                 }, ckp_path)
                     
                 pbar.update(1)
@@ -84,48 +84,49 @@ def train_on_policy_agent(env, agent, s_epoch, total_epoch, s_episode, total_epi
             
     agent.actor.load_state_dict(actor_best_weight)
     agent.critic.load_state_dict(critic_best_weight)
-            
-    return reward_list
+    
+    # 如果检查点保存了回报列表, 就无需返回      
+    # return return_list
 
-def train_off_policy_agent(env,  agent,  s_epoch, total_epoch, s_episode,  total_episode,  replay_buffer,
-                           minimal_size,  batch_size, reward_list, ckp_path):
+def train_off_policy_agent(env,  agent,  s_epoch, total_epochs, s_episode,  total_episodes,  replay_buffer,
+                           minimal_size,  batch_size, return_list, ckp_path):
     '''
-    离线策略, 从经验池抽取
+    离线策略, 从经验池抽取, 仅限演员评论员框架
     '''
     best_score = 0
-    reward_list = []
-    for epoch in range(s_epoch, total_epoch):
-        with tqdm(total=(total_episode - s_episode), desc='<%d/%d>' % (epoch + 1, total_epoch), leave=False) as pbar:
-            for episode in range(s_episode, total_episode):
-                episode_reward = 0
-                state = env.reset()[0]
+    return_list = []
+    for epoch in range(s_epoch, total_epochs):
+        with tqdm(total=(total_episodes - s_episode), desc='<%d/%d>' % (epoch + 1, total_epochs), leave=False) as pbar:
+            for episode in range(s_episode, total_episodes):
+                episode_return = 0
+                state = env.reset(seed=42)[0]
                 done = truncated = False
                 while not (done | truncated):
                     action = agent.take_action(state)
                     next_state, reward, done, truncated, _ = env.step(action)
                     replay_buffer.add(state, action, reward, next_state, done, truncated)
                     state = next_state
-                    episode_reward += reward
+                    episode_return += reward
                     if replay_buffer.size() > minimal_size:
                         b_s, b_a, b_r, b_ns, b_d, b_t = replay_buffer.sample(batch_size)
                         transition_dict = {'states': b_s, 'actions': b_a, 'next_states': b_ns, 'rewards': b_r, 'dones': b_d, 'truncated': b_t}
                         agent.update(transition_dict)
-                reward_list.append(episode_reward)
+                return_list.append(episode_return)
                 if (episode + 1) % 10 == 0:
-                    pbar.set_postfix({'episode': '%d' % (total_episode * epoch + episode + 1),
-                                      'recent_return': '%.3f' % np.mean(reward_list[-10:])})
+                    pbar.set_postfix({'episode': '%d' % (total_episodes * epoch + episode + 1),
+                                      'recent_return': '%.3f' % np.mean(return_list[-10:])})
                     
-                if episode_reward > best_score:
+                if episode_return > best_score:
                     actor_best_weight = agent.actor.state_dict()
                     critic_best_weight = agent.critic.state_dict()
-                    best_score = episode_reward
+                    best_score = episode_return
                     
                 torch.save({
                 'epoch': epoch,
                 'episode': episode,
                 'actor_best_weight': actor_best_weight,
                 'critic_best_weight' : critic_best_weight,
-                'reward_list': reward_list,
+                'return_list': return_list,
                 }, ckp_path)
                 
                 pbar.update(1)
@@ -133,9 +134,9 @@ def train_off_policy_agent(env,  agent,  s_epoch, total_epoch, s_episode,  total
             
     agent.actor.load_state_dict(actor_best_weight)
     agent.critic.load_state_dict(critic_best_weight)
-        
-    return reward_list
-
+    
+    # 如果检查点保存了回报列表, 就无需返回      
+    # return return_list
 
 def compute_advantage(gamma, lmbda, td_delta):
     td_delta = td_delta.detach().numpy()
@@ -152,29 +153,29 @@ def show_gym_policy(name, model, render_mode="human", epochs=10, steps=300):
     env.reset()
     totals = []
     for i in range(epochs):  # 测试轮数
-        episode_rewards = 0
-        obs = env.reset()[0]  # 第二个输出为info，可以不要
+        episode_returns = 0
+        obs = env.reset(seed=42)[0]  # 第二个输出为info，可以不要
         for _ in range(steps):  # 每回合最多300步
             try:
                 Q_values = model(torch.tensor(obs).to('cuda'))
                 action = np.argmax(Q_values.tolist())
                 obs, reward, done, truncated, info = env.step(action)
-                episode_rewards += reward
+                episode_returns += reward
             except:
                 env.close()
                 raise Exception('Action execution error!')
             if done or truncated:
                 break
-        totals.append((episode_rewards))
+        totals.append((episode_returns))
     env.close()
     return totals
 
-def picture_reward(reward_list, policy_name, env_name, move_avg=9):
+def picture_reward(return_list, policy_name, env_name, move_avg=9):
     '''传入回报列表, 策略名称, 环境名称, 移动平均周期'''
     sns.set()
-    episodes_list = list(range(len(reward_list)))
-    mv_return = moving_average(reward_list, move_avg)
-    plt.plot(episodes_list, reward_list, label='origin', linestyle='-', alpha=0.5)
+    episodes_list = list(range(len(return_list)))
+    mv_return = moving_average(return_list, move_avg)
+    plt.plot(episodes_list, return_list, label='origin', linestyle='-', alpha=0.5)
 
     plt.plot(episodes_list, mv_return, label='avg 9', linewidth='1.5')
     plt.title('{} on {}'.format(policy_name, env_name))
